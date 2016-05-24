4fbb2d34dbd12be692d541103b1b073ac30485e7
Priyesha Yadav <priyesha.yadav@impetus.co.in>
2016-05-24 16:53:52 +0530

    postreceivetest

--- src/test/testHelloWorld/TestHelloWorld.java
+++ src/test/testHelloWorld/TestHelloWorld.java
@@ -16,7 +16,7 @@ public class TestHelloWorld {
 		String str = obj.funct1();
 
 		Assert.assertNotNull(str);
-		Assert.assertEquals(str, "triggering the builds");
+		Assert.assertEquals(str, "triggering the builds on commit");
 
 	}
 
